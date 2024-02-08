1291e4a3e9d7eed8a566b2fb8836208ddf496f49
bilal-sagir <sagir.bilal@icloud.com>
2024-02-08 03:27:09 +0300

    Initial Commit

---
+++ HikeAppSwiftUI/HikeAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeAppSwiftUIApp.swift
+//  HikeAppSwiftUI
+//
+//  Created by Bilal on 8.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct HikeAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
